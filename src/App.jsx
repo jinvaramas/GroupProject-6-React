d0957f7a583f9d219a92dd7088398f19c72afa3e
jinvaramas <Jinvaramas@gamil.com>
2026-05-05 13:30:42 +0700

    addcomponents

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
 import './App.css';
+import flairBanner from './assets/Flair (2).png';
+import Head from './components/head';
+import Nav from './components/Nav';
+import Banner from './components/banner';
 
 export default function App() {
   return (
-    <>
-    </>
+    <div className="flair-banner">
+      <img src={flairBanner} alt="Flair banner" />
+      <div className="flair-overlay">
+        <Head />
+        <Nav />
+      </div>
+      <Banner />
+    </div>
   );
 }
